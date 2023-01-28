2b91ce117081667a0a49edc6f5a3cbcbd94c0579
Akshay <akshay0127.be21@chitkara.edu.in>
2023-01-28 13:34:10 +0530

    added Expense app basic design

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:expenseapp/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List<Transaction> transaction = [
+    Transaction(
+      id: "1",
+      title: "Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "2",
+      title: "Specs",
+      amount: 29.99,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Colors.blue,
+            child: Container(width: double.infinity, child: Text("Chart")),
+          ),
+          Column(
+            children: transaction.map((txt) {
+              return Card(
+                child: Text(txt.title),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
